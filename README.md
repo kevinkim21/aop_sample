bafd3234c36a965133fbb0778060ffe9ca56bf65
kevinkim21 <dreamworld612@gmail.com>
2023-11-04 02:26:23 +0900

    add kafka

---
+++ README.md
@@ -0,0 +1,26 @@
+# Getting Started
+
+### Reference Documentation
+
+For further reference, please consider the following sections:
+
+* [Official Gradle documentation](https://docs.gradle.org)
+* [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/3.1.5/gradle-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/3.1.5/gradle-plugin/reference/html/#build-image)
+* [Spring Configuration Processor](https://docs.spring.io/spring-boot/docs/3.1.5/reference/htmlsingle/index.html#appendix.configuration-metadata.annotation-processor)
+* [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/3.1.5/reference/htmlsingle/index.html#using.devtools)
+* [Spring Data MongoDB](https://docs.spring.io/spring-boot/docs/3.1.5/reference/htmlsingle/index.html#data.nosql.mongodb)
+* [Spring for Apache Kafka](https://docs.spring.io/spring-boot/docs/3.1.5/reference/htmlsingle/index.html#messaging.kafka)
+
+### Guides
+
+The following guides illustrate how to use some features concretely:
+
+* [Accessing Data with MongoDB](https://spring.io/guides/gs/accessing-data-mongodb/)
+
+### Additional Links
+
+These additional references should also help you:
+
+* [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
+
--- src/main/java/com/example/demo/service/TraceService.java
+++ src/main/java/com/example/demo/service/TraceService.java
@@ -1,15 +1,39 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.TraceDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TraceService {
+    private final Environment environment;
+    private final KafkaTemplate<String,String> devKafkaTemplate;
+    private final KafkaTemplate<String,String> prodKafkaTemplate;
 
     public void save(TraceDto trace) {
         log.info("====== TRACE ======");
         log.info("trace: {}", trace);
+        this.getKafkaTemplate().send("trace", trace.toString());
+    }
+
+    private KafkaTemplate<String, String> getKafkaTemplate() {
+        // 현재 활성화된 프로필들을 가져옵니다.
+        String[] activeProfiles = environment.getActiveProfiles();
+        // 활성화된 프로필에 따라 적절한 KafkaTemplate을 선택합니다.
+        // 예를 들어, "dev" 또는 "prod" 프로필을 확인하고 해당하는 KafkaTemplate을 반환합니다.
+        for (String profile : activeProfiles) {
+            if (profile.equalsIgnoreCase("dev")) {
+                return devKafkaTemplate;
+            }
+            if(profile.equalsIgnoreCase("prod")) {
+                return prodKafkaTemplate;
+            }
+        }
+        return prodKafkaTemplate;
     }
 }
